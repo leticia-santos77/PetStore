f6b8881c8ee32ee59db7b5251a62bb53667408e4
RAfael <rafaels.zanella@gmail.com>
2020-02-28 17:27:37 -0300

    feat(api): adicionar api.

---
+++ api/reservaderecursos/src/main/java/br/com/dbccompany/reservaderecursos/ReservaderecursosApplication.java
@@ -0,0 +1,13 @@
+package br.com.dbccompany.reservaderecursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaderecursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaderecursosApplication.class, args);
+	}
+
+}
