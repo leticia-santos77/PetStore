d54bd8ee79646b1ccb98bde4be347d4b133b97fb
amanda santos <amanda.santos@dbccompany.com.br>
2020-02-29 16:54:22 -0300

    feat(login)WIP:add login routes.

---
+++ resource-reservation/src/pages/login/login.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+export default class login extends Component{
+    constructor(){
+        super();
+        this.state = {
+            email:'',
+            password:'',
+            access:false            
+        }
+    }
+    render(){
+        return (
+            <React.Fragment>
+                <h2>Reserva de Recurso</h2>
+                <input name="userName" placeholder="usuário"></input>
+                <input name="password" type="password" placeholder="senha"></input>
+                <button>ENTRAR</button>
+            </React.Fragment>
+        )
+    }
+}
