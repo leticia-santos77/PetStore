d2e5d1fe030e3373f6982d53705119269641ae02
Fabio Batista <fabio.batista@dbccompany.com>
2020-03-03 14:20:51 -0300

    feat(login): add style.

--- resource-reservation/src/pages/login/Login.jsx
+++ resource-reservation/src/pages/login/Login.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './login.css';
 import Logo from './logoECOS.png';
+import Input from '../../components/input/Input';
 
 export default class login extends Component {
     constructor() {
@@ -16,14 +17,14 @@ export default class login extends Component {
             <div className="container">
                 <div className="box">
                     <div className="div-logo" >
-                        <img  className="logo" src={Logo}/>
+                        <img alt="logo" className="logo" src={ Logo }/>
                     </div>
                     <div className="div-login div-login-text">
                         <h2>Reserva de Recurso</h2>
                     </div>
                     <div className="div-login div-login-input">
-                        <input className="input-login" name="userName" placeholder="usuário"></input>
-                        <input className="input-login" name="password" type="password" placeholder="senha"></input>
+                        <Input className="input-login" name="userName" placeholder="usuário"/>
+                        <Input className="input-login" name="password" type="password" placeholder="senha"/>
                     </div>
                     <div className="div-login div-login-button">
                         <button className="button-login">ENTRAR</button>
