e79ca978b71ca4484e32bed534002c6fa5bc0024
Gabriel Eugenio <gabriel.eugenio@dbccompany.com.br>
2020-03-04 17:32:12 -0300

    feat(card):wip.

--- resource-reservation/src/components/card/Card.jsx
+++ resource-reservation/src/components/card/Card.jsx
@@ -5,5 +5,11 @@ export default ( props )=>
         <div>
             {props.children}
         </div>
-      
+        <ul className="tirar">
+                    <li><i class="fas fa-campground"></i>nome</li>
+                    <li><i class="fas fa-users"></i>numero</li>
+                    <li><i class="fas fa-tv"></i>Televisão</li>
+                    <li><i class="far fa-check-square"></i>sala</li>    
+                    <li><i class="far fa-calendar-alt"></i>data</li>
+                </ul>
 </section>
