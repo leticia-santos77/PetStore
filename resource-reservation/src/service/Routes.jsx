3ded8fbedc0a354e4410f8aa67f09a46e153e2b1
Rafael S. Zanella <rafaels.zanella@gmail.com>
2020-03-08 01:23:44 -0300

    feat(Routes, Sidebar): create new menu links and fix routes.

--- resource-reservation/src/service/Routes.jsx
+++ resource-reservation/src/service/Routes.jsx
@@ -8,12 +8,14 @@ import Login from '../pages/login/Login';
 import Home from '../pages/home/Home';
 import ListResources from '../pages/resources/ListResources'
 import ListBookings from '../pages/bookings/ListBookings'
+import Calendar from '../components/calendar/Calendar.jsx';
  const Routes = () =>(
     <BrowserRouter>
         <Switch>
             <Route exact path='/'  component={ IsAuthenticated() ? Home : Login } />
-            <Route path='/recursos' component={ListResources}/>
-            <Route path='/reservas' component={ListBookings}/>
+            <Route path='/recursos' component={ ListResources }/>
+            <Route path='/reservas' component={ ListBookings }/>
+            <Route path='/calendario' component={ Calendar } />
             <PrivateRoute exact path='/home'  component={ Home } />
         </Switch>
     </BrowserRouter>
