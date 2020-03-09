420490fd05c97604607be530ca39986cac2e5b05
Rafael S. Zanella <rafaels.zanella@gmail.com>
2020-03-08 23:53:06 -0300

    feat(Routes): add 404 not foud page.

--- resource-reservation/src/service/Routes.jsx
+++ resource-reservation/src/service/Routes.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
 import PrivateRoute from './PrivateRoute.jsx';
 import { isAuthenticated } from './Auth';
-
 import Login from '../pages/login/Login';
 import Home from '../pages/home/Home';
 import ListResources from '../pages/resources/ListResources'
 import ListBookings from '../pages/bookings/ListBookings'
-
 import CalendarPage from '../pages/calendar/CalendarPage.jsx';
+import NotFound from '../pages/NotFound/NotFound.jsx';
+
 const Routes = () => (
     <BrowserRouter>
         <Switch>
-            <Route exact path='/' component={isAuthenticated() ? Home : Login} />
+            <Route path='/' exact component={isAuthenticated() ? Home : Login} /> 
+            <Route path='/login' exact component={isAuthenticated() ? Home : Login} /> 
             <PrivateRoute path='/recursos' component={ListResources} />
             <PrivateRoute path='/reservas' component={ListBookings} />
             <PrivateRoute path='/calendario' component={CalendarPage} />
-            <PrivateRoute exact path='/home' component={Home} />
+            <PrivateRoute path='/home' exact component={Home} />
+            <Route component={NotFound} />
         </Switch>
     </BrowserRouter>
 );
